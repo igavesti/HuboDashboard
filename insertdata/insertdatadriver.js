9070e1e1fd83fb4fadd125322407da2bf4cb6842
Igavesti <albert.frebryanto@gmail.com>
2020-11-14 14:54:18 +0700

    init datepicker

--- insertdata/insertdatadriver.js
+++ insertdata/insertdatadriver.js
@@ -23,4 +23,20 @@ function insertdata() {
 	));
 }
 
+$(function () {
+
+    // INITIALIZE DATEPICKER PLUGIN
+    $('.datepicker').datepicker({
+        clearBtn: true,
+        format: "dd/mm/yyyy"
+    });
+
+
+    // FOR DEMO PURPOSE
+    $('#reservationDate').on('change', function () {
+        var pickedDate = $('input').val();
+        $('#pickedDate').html(pickedDate);
+    });
+});
+
 insert_button.onclick = insertdata;
